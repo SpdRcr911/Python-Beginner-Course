bd96c60ead8f59c2648030c28c10416b7c36f840
Rafael Delgado <rafael@delgado.com>
2019-01-20 21:19:49 -0800

    Update lesson 1

--- Lesson01/Exercise01.py
+++ Lesson01/Exercise01.py
@@ -1,11 +1,11 @@
 def replacer(strIn):
     return None
 
-def captilizeAll(strIn):
-    return None
-
 def numberToWord(numIn):
     return None
 
 def countWords(strIn):
+    return None
+
+def captilizeAll(strIn):
     return None
